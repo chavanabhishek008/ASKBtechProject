f262a1d76b8eadd43fd8ed39689c91d6b8872bf6
chavanabhishek008 <66113005+chavanabhishek008@users.noreply.github.com>
2021-01-19 22:18:55 +0530

    Add files via upload

---
+++ Form_success.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const form_success = () => {
+    return (
+        <div className="form-content-right">
+          <div className="form-success">Data saved successfully</div>  
+        </div>
+    )
+}
+export default form_success;
